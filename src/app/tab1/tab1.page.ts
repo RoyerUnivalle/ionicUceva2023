18018be7414b45243b9582492ed93d318263ea61
r3g0r <royer.estrada@correounivalle.edu.co>
2023-03-21 11:44:26 -0500

    ahora en tab 4 si se puede acceder a un boton, hay un lio con el app-container

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { EventsService } from '../services/events.service';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,8 +10,11 @@ import { EventsService } from '../services/events.service';
 export class Tab1Page {
 
   public data:any = [];
+  public handlerMessage= '';
+  public roleMessage = '';
 
-  constructor(private eventsService:EventsService) {
+  constructor(private eventsService:EventsService,
+    private alertController: AlertController) {
 
     console.log('Hola tab 1');
 
@@ -32,5 +37,32 @@ export class Tab1Page {
             console.log(error);
           }
         );
+    }
+
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Alert!',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              this.handlerMessage = 'Alert canceled';
+            },
+          },
+          {
+            text: 'OK',
+            role: 'confirm',
+            handler: () => {
+              this.handlerMessage = 'Alert confirmed';
+            },
+          },
+        ],
+      });
+  
+      await alert.present();
+  
+      const { role } = await alert.onDidDismiss();
+      this.roleMessage = `Dismissed with role: ${role}`;
     }
   }
--- src/app/tab4/tab4.component.ts
+++ src/app/tab4/tab4.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -7,12 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Component  implements OnInit {
 
-  constructor() { }
+  public handlerMessage= '';
+  public roleMessage = '';
+
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {}
 
   saludar(){
   console.log('hola te saludo')
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.handlerMessage = 'Alert canceled';
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.handlerMessage = 'Alert confirmed';
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
   }
 
 }
